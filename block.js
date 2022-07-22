82bf8753515ef7bf753c77e56ddc344dca475cc8
d0bl3c3r0 <matias@everythink.ai>
2022-07-21 22:00:54 -0300

    adding blockchain + blocks

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256')
 class Block {
 	constructor(timestamp, lastHash, hash, data) {
 		this.timestamp = timestamp;
@@ -11,8 +12,11 @@ class Block {
 	static mineBlock(lastBlock, data){
 		const timestamp = Date.now();
 		const lastHash  = lastBlock.hash;
-		const hash      = 'todo-hash';
+		const hash      = Block.hash(timestamp, lastHash, data);
 		return new this(timestamp,lastHash,hash,data);
+	}
+	static hash(timestamp, lastHash, data){
+		return SHA256(`${timestamp}${lastHash}${data}`).toString();
 	}
 	toString() {
 		return `Block -
