99c3087e68e57e09e62e1ddf536a5fdff6aba9e0
d0bl3c3r0 <matias@everythink.ai>
2022-07-22 12:08:39 -0300

    Jest: transaction verification
    we did 2 tests to verify the integrity of the balance and the integrity of the next amount

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -40,4 +40,20 @@ describe('Transaction', () => {
 		transaction.outputs[0].amount = 50000;
 		expect(Transaction.verifyTransaction(transaction)).toBe(false);
 	})
+	describe('and updating a transaction',()=>{
+		let nextAmount, nextRecipient;
+		beforeEach(()=>{
+			nextAmount    = 20;
+			nextRecipient = "next=4ddr355",
+			transaction   = transaction.update(wallet, nextRecipient, nextAmount);
+		});
+		it(`subtracts the next amount from the senders output`,()=> {
+			expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+			.toEqual(wallet.balance - amount - nextAmount);
+		});
+		it('outputs an amount for the next recipient', () => {
+			expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+			.toEqual(nextAmount);
+		});
+	});
 });
