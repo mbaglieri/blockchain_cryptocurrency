db7fd55f33eece92b2543e48f624b9fa7161a322
d0bl3c3r0 <matias@everythink.ai>
2022-07-21 21:33:51 -0300

    added genesis block

--- block.js
+++ block.js
@@ -1,16 +1,21 @@
 class Block {
-	constructor(timestamp, lastHash, hash, data){
+	constructor(timestamp, lastHash, hash, data) {
 		this.timestamp = timestamp;
 		this.lastHash  = lastHash;
 		this.hash      = hash;
 		this.data      = data;
 	}
-	toString(){
+	static genesis(){
+		return new this('Genesis time','-----','first-h45s',[]);
+	}
+	toString() {
 		return `Block -
 			Timestamp: ${this.timestamp}
 			Last Hash: ${this.lastHash}
 			Hash     : ${this.hash}
-			Data     : ${this.data}`;
+			Data     : ${this.data}
+		`;
 	}
+
 }
 module.exports = Block;
